06844aea9efe70ed42de3986bb35e0c2771f711c
Alexey <treshaalesha@gmail.com>
2017-03-31 00:20:18 +0300

    Feat: fixed sizes of popUp modal

--- src/modules/popUpModal/components/PopUpModal.tsx
+++ src/modules/popUpModal/components/PopUpModal.tsx
@@ -2,19 +2,21 @@ import * as React from 'react';
 import { render } from 'react-dom';
 import * as Modal from 'react-modal';
 import LastDraft from '../../DraftEditor/LastDraft';
-import customStyles from '../styles/style';
+import { customStyles } from '../styles/style';
 
-export const PopUpModal = (props) => (
-  <div>
-    <button onClick={props.changeModalStatus}>Open Modal</button>
-    <Modal
-      isOpen={props.isModalOpen}
-      onAfterOpen={this.afterOpenModal}
-      onRequestClose={props.changeModalStatus}
-      style={customStyles}
-      contentLabel="Example Modal"
-    >
-      <LastDraft/>
-    </Modal>
-  </div>
-);
+export const PopUpModal = (props) => {
+  return (
+    <div>
+      <button onClick={props.changeModalStatus}>Open Modal</button>
+      <Modal
+        isOpen={props.isModalOpen}
+        onAfterOpen={this.afterOpenModal}
+        onRequestClose={props.changeModalStatus}
+        style={customStyles}
+        contentLabel="Example Modal"
+      >
+        <LastDraft/>
+      </Modal>
+    </div>
+  );
+}
--- src/modules/popUpModal/styles/style.ts
+++ src/modules/popUpModal/styles/style.ts
@@ -1,9 +1,12 @@
-export const content =  {
-  top: '50%',
-  left: '50%',
-  right: 'auto',
-  bottom: 'auto',
-  marginRight: '-50%',
-  transform: 'translate(-50%, -50%)',
-  overflow: 'scroll'
-}
+export const customStyles = {
+  content : {
+    top: '50%',
+    left: '50%',
+    right: 'auto',
+    bottom: 'auto',
+    marginRight: '-50%',
+    transform: 'translate(-50%, -50%)',
+    height: '100%', // <-- This sets the height
+    overflow: 'scroll' // <-- This tells the modal to scrol
+  }
+};
