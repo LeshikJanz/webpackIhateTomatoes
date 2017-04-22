4b6f87c05897b0243c449f4e43dc5fae1455b40d
Alexey <treshaalesha@gmail.com>
2017-04-22 15:18:19 +0300

    Feat: added simple routes

--- src/routes.js
+++ src/routes.js
@@ -1,15 +1,17 @@
 import React from 'react';
 import { Route, IndexRoute } from 'react-router';
 import App from './modules/main/containers/index.ts';
-import { Header } from "modules/header/containers";
+import Header from "modules/header/containers";
+import Base from "./modules/main/components/Base/Base";
 
 export const urls = {
   index: '/',
-  header: 'header'
+  header: 'header',
 };
 
 export const routes = (
-  <Route path={urls.index} component={App}>
-    <Route path={urls.header} component={Header}></Route>
-  </Route>
+    <Route path={urls.index} component={Base}>
+      <IndexRoute component={App}></IndexRoute>
+      <Route path={urls.header} component={Header}></Route>
+    </Route>
 );
