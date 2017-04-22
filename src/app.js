34fddc5e2d2ee78083e0c8a70ffb3db086375100
Alexey <treshaalesha@gmail.com>
2017-04-22 16:43:39 +0300

    Feat: updated router middleware

--- src/app.js
+++ src/app.js
@@ -2,20 +2,22 @@ import 'babel-polyfill';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import App from './modules/main/containers/index.ts';
 import { createStore, applyMiddleware } from 'redux'
 import createSagaMiddleware from 'redux-saga'
 import rootSaga from './modules/sagas';
 import reducer from '../reducers';
-import { Router, Route, hashHistory } from 'react-router';
+import { Router, hashHistory } from 'react-router';
 import { syncHistoryWithStore, routerMiddleware } from 'react-router-redux';
 import { routes } from 'modules/routes';
 
 const sagaMiddleware = createSagaMiddleware();
-const routermiddleware = routerMiddleware(hashHistory);
 
-const store = createStore(reducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(), applyMiddleware(sagaMiddleware, routermiddleware))
+const store = createStore(reducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+  applyMiddleware(sagaMiddleware, routerMiddleware(hashHistory))
+);
 const history = syncHistoryWithStore(hashHistory, store);
+
 sagaMiddleware.run(rootSaga);
 
 ReactDOM.render(
