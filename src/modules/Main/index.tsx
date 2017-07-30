9da73cfc621035116ac8c7e4794c7fb635f66c2a
leshikjanz <alex.tereshko@softeq.com>
2017-07-30 11:58:08 +0300

    Feat: added link for get started button to get redirected to registration page

--- src/modules/Main/index.tsx
+++ src/modules/Main/index.tsx
@@ -1,5 +1,8 @@
 import * as React from "react";
 import { Cloud } from "../Cloud/components/Cloud";
+import { Link } from 'react-router';
+
+import { urls } from "../urls";
 require('./styles/style.scss');
 
 export const Base = (props) => (
@@ -10,16 +13,18 @@ export const Base = (props) => (
 /**
  * Main page
  */
-export const Main = ( props ) => (
+export const Main = (props) => (
   <div>
     <div className="centered-container welcome-text">
       <h2>Big Head is a perfect tool for preservation
        your own experience and exchange of
        knowledge with people from all over the world</h2>
       <div>
-        <button className="primary get-started-button">
-          Get Started!
-        </button>
+        <Link to={ urls.registration }>
+          <button className="primary get-started-button">
+            Get Started!
+          </button>
+        </Link>
       </div>
     </div>
     <div className="blur" style={{ pointerEvents: 'none' }}>
