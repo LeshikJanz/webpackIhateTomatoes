7bbcb4d5c3c079d833551abaa1bf15152714f075
leshikjanz <treshaalesha@gmail.com>
2017-08-30 14:33:12 +0300

    Feat: implemented zoom label

--- src/modules/Sky/components/ZoomPanel.tsx
+++ src/modules/Sky/components/ZoomPanel.tsx
@@ -2,16 +2,21 @@ import * as React from 'react';
 
 const ZOOM_MULTIPLIER = 1.3;
 
-export const ZoomPanel = ({ zoom, handleZoom }) => (
+export const ZoomPanel = ( { zoom, handleZoom } ) => (
   <div className="zoom-panel">
-    <button onClick={() => handleZoom(zoom * ZOOM_MULTIPLIER)}><img
-      src="assets/img/Zoom-plus-min.png" alt="Zoom +"/>
-    </button>
-    <button onClick={() => handleZoom(zoom / ZOOM_MULTIPLIER)}>
-      <img src="assets/img/Zoom-minus-min.png" alt="Zoom -"/>
-    </button>
-    <button onClick={() => handleZoom(1)}>
-      <img src="assets/img/Zoom-reset.png" alt="Reset zoom"/>
-    </button>
+    <div>
+      <button disabled={zoom >= 10}
+              onClick={() => handleZoom(zoom * ZOOM_MULTIPLIER)}>
+        <img src="assets/img/Zoom-plus-min.png" alt="Zoom +"/>
+      </button>
+      <button disabled={zoom <= 0.1}
+              onClick={() => handleZoom(zoom / ZOOM_MULTIPLIER)}>
+        <img src="assets/img/Zoom-minus-min.png" alt="Zoom -"/>
+      </button>
+      <button onClick={() => handleZoom(1)}>
+        <img src="assets/img/Zoom-reset.png" alt="Reset zoom"/>
+      </button>
+    </div>
+    <h4>Zoom: {zoom.toFixed(1)}</h4>
   </div>
 );
--- src/modules/Sky/components/index.tsx
+++ src/modules/Sky/components/index.tsx
@@ -11,29 +11,31 @@ import ZoomPanel from "../containers/zoomContainer";
 import { OPEN_BUTTON_HEIGHT, OPEN_BUTTON_WIDTH, VIEW_CONTAINER_HEIGHT } from "../constants/index";
 const ResponsiveReactGridLayout = WidthProvider(Responsive);
 
-export const GridLayout = ({ sky, modal, params, handleModal, handleCloudFormSubmit, updateLayout, zoom }) => {
+export const GridLayout = ( { sky, modal, params, handleModal, handleCloudFormSubmit, updateLayout, zoom } ) => {
   const actionMenu: IMenu[] = [
     { callback: 'handleModal', arg: 'CloudAdd', placeholder: 'Create cloud', icon: 'fa fa-menu fa-cloud' },
     { callback: 'handleSettings', placeholder: 'Settings', icon: 'fa fa-menu fa-cog' }
   ];
 
-  const fitByWidth = (element: HTMLElement) => {
+  const fitByWidth = ( element: HTMLElement ) => {
     const nameBlock = element.querySelector(".name");
     const openCloudButton = element.querySelector(".open-cloud");
     const reviewsCounter = element.querySelector(".reviews-counter");
     const goalBlock = element.querySelector(".goal");
+    const deleteIcon = element.querySelector(".delete-icon");
 
     if ( element.offsetWidth - nameBlock.offsetWidth <= OPEN_BUTTON_WIDTH ) {
-      openCloudButton.style.display = reviewsCounter.style.display = goalBlock.style.display = 'none';
+      [openCloudButton, reviewsCounter, goalBlock, deleteIcon].forEach(e => e.style.display = 'none');
     } else {
-      openCloudButton.style.display = reviewsCounter.style.display = goalBlock.style.display = 'flex';
+      [openCloudButton, reviewsCounter, goalBlock, deleteIcon].forEach(e => e.style.display = '');
     }
   };
 
-  const fitByHeight = (element: HTMLElement) => {
+  const fitByHeight = ( element: HTMLElement ) => {
     const itemFooter = element.querySelector(".item-footer");
     const nameBlock = element.querySelector(".name");
     const goalBlock = element.querySelector(".goal");
+    const deleteIcon = element.querySelector(".delete-icon");
 
     if ( element.offsetHeight - goalBlock.offsetHeight - VIEW_CONTAINER_HEIGHT <= OPEN_BUTTON_HEIGHT ) {
       goalBlock.style.display = 'none';
@@ -42,13 +44,13 @@ export const GridLayout = ({ sky, modal, params, handleModal, handleCloudFormSub
     }
 
     if ( element.offsetHeight - nameBlock.offsetHeight <= OPEN_BUTTON_HEIGHT ) {
-      itemFooter.style.display = 'none';
+      [itemFooter, deleteIcon].forEach(e => e.style.display = 'none');
     } else {
-      itemFooter.style.display = 'flex';
+      [itemFooter, deleteIcon].forEach(e => e.style.display = '');
     }
   };
 
-  const handleSize = (element: HTMLElement) => {
+  const handleSize = ( element: HTMLElement ) => {
     if ( element ) {
       fitByWidth(element);
       fitByHeight(element);
@@ -60,13 +62,13 @@ export const GridLayout = ({ sky, modal, params, handleModal, handleCloudFormSub
       <ResponsiveReactGridLayout className="layout"
                                  layout={sky.layout}
                                  onLayoutChange={updateLayout}
-                                 onResize={(l, o, n, p, e, element) => handleSize(element.parentNode)}
+                                 onResize={( l, o, n, p, e, element ) => handleSize(element.parentNode)}
                                  breakpoints={{ lg: 1200, md: 996, sm: 768, xs: 480, xxs: 0 }}
                                  cols={{ lg: 12 / zoom, md: 8 / zoom, sm: 4 / zoom, xs: 2 / zoom, xxs: 1 }}
                                  rowHeight={100 * zoom}
       >
         {
-          sky.clouds.map((c: ICloud) =>
+          sky.clouds.map(( c: ICloud ) =>
             <div key={c.id} data-grid={ sky.layout.find(l => l.i === c.id) || { x: 0, y: 0, w: 2, h: 2 } }
                  ref={handleSize}>
               <SkyItem cloud={c}/>
@@ -75,7 +77,7 @@ export const GridLayout = ({ sky, modal, params, handleModal, handleCloudFormSub
       </ResponsiveReactGridLayout>
       {
         !params.id &&
-        <GooeyMenu onSelect={ (callback, arg) => eval(callback)(arg) } menuItems={actionMenu}/>
+        <GooeyMenu onSelect={ ( callback, arg ) => eval(callback)(arg) } menuItems={actionMenu}/>
       }
       <CustomModal
         title="Adding cloud"
