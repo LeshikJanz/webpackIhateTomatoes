a9b28a44ec0dbef029e600e05d3dc145fa9d0649
leshikjanz <treshaalesha@gmail.com>
2017-10-04 15:38:38 +0300

    Refactor: added mobile styles for navigation menu

--- src/modules/Main/navigation/components/index.tsx
+++ src/modules/Main/navigation/components/index.tsx
@@ -6,11 +6,11 @@ const SVG = require('react-svg');
 
 export const Navigation = ({ goToUrl, goToBoard }) => (
   <div className="navigation-container">
-    <Link to={urls.index}><SVG path="assets/icons/hamburger.svg" className="nav-hamburger"/></Link>
+    <SVG path="assets/icons/hamburger.svg" className="nav-hamburger"/>
     <nav>
-      <Link to={urls.research} activeClassName="active">Find&nbsp;new</Link><br/>
+      <Link to={urls.research} activeClassName="active">Find&nbsp;new</Link>
       <Link to={urls.board} activeClassName="active" onClick={goToBoard}>Board</Link>
-      <Link to={urls.users} activeClassName="active">Users</Link><br/>
+      <Link to={urls.users} activeClassName="active">Users</Link>
       <Link to={urls.settings} activeClassName="active">Settings</Link>
     </nav>
   </div>
